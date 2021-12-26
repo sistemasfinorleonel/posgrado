f54b3188e3f3cb438e9b11557cd34e990e5c26c0
luisbeltrancondori <luisbeltrancondoricano@gmail.com>
2021-12-26 13:39:55 -0400

    Estudiante

---
+++ database/migrations/2021_12_26_173216_create_estudiantes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstudiantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estudiantes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apPaterno');
+            $table->string('apMaterno');
+            $table->string('mail');
+            $table->string('telefono');
+            $table->string('direccion');
+            $table->unsignedBigInteger('requisito_id');
+            $table->foreign('requisito_id')->reference('id')->on('requisitos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estudiantes');
+    }
+}
