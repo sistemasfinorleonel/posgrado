e10873731930ae288626439e88411d0f98aeb7b1
luisbeltrancondori <luisbeltrancondoricano@gmail.com>
2021-12-28 10:51:46 -0400

    Video2 Completo funcionando

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<x-app-layout>
+    
+</x-app-layout>
+
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +134,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
