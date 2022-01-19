aaef0b2c925e298110a93f51fbf93ca0ba2d3615
luisbeltrancondori <luisbeltrancondoricano@gmail.com>
2022-01-18 22:59:04 -0400

    Formulario para actualizar curso

--- resources/views/livewire/administrador/programas-index.blade.php
+++ resources/views/livewire/administrador/programas-index.blade.php
@@ -60,7 +60,7 @@
                                                 Admin
                                             </td>
                                             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                                <a href="{{route('administrador.programas.edit', $programa)}}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                                             </td>
                                     </tr>
                                 @endforeach
