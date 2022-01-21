32a6d8c03a62a9b394dd2244f26e8949af74199f
sistemasfinorleonel <ingenierosistemasfinor@gmail.com>
2022-01-21 15:11:18 -0400

    s
    se

--- database/seeders/EstudianteSeeder.php
+++ database/seeders/EstudianteSeeder.php
@@ -14,29 +14,28 @@ class EstudianteSeeder extends Seeder
      */
     public function run()
     {
-        Estudiante::create(['Nregistro'=>'2170829','nombre'=>'Leonel','paterno'=>'Chana','materno'=>'Cruz','email'=>'alv@gmail.com','telefono'=>'2432423','direccion'=>'Barrio Monasterio']);
-        Estudiante::create(['Nregistro'=>'2170839','nombre'=>'Lurdes','paterno'=>'Contreras','materno'=>'Alvares','email'=>'alv1@gmail.com','telefono'=>'3542354','direccion'=>'Barrio Guadalupe 2']);
-        Estudiante::create(['Nregistro'=>'2170821','nombre'=>'Jazmin','paterno'=>'Leon','materno'=>'Castillo','email'=>'alv2@gmail.com','telefono'=>'235243','direccion'=>'Barrio Aleman calle 2']);
-        Estudiante::create(['Nregistro'=>'2170822','nombre'=>'Noelia','paterno'=>'Duarte','materno'=>'Castro','email'=>'alv3@gmail.com','telefono'=>'235323','direccion'=>'Barrion Rincon del tigre']);
-        Estudiante::create(['Nregistro'=>'2170823','nombre'=>'Julio','paterno'=>'Guzman','materno'=>'Mendez','email'=>'alv4@gmail.com','telefono'=>'3453232','direccion'=>'Zona sur Montero']);
-        Estudiante::create(['Nregistro'=>'2170824','nombre'=>'Tereza','paterno'=>'Espinoza','materno'=>'Alvarez','email'=>'alv5@gmail.com','telefono'=>'252342','direccion'=>'Calle ingavi']);
-        Estudiante::create(['Nregistro'=>'2170825','nombre'=>'Marcos','paterno'=>'Condori','materno'=>'Andia','email'=>'alv6@gmail.com','telefono'=>'243523','direccion'=>'Zona los Chacos']);
-        Estudiante::create(['Nregistro'=>'2170826','nombre'=>'Selena','paterno'=>'Nina','materno'=>'Ribera','email'=>'alv7@gmail.com','telefono'=>'23523','direccion'=>'Santa Cruz, zona Chacos']);
-        Estudiante::create(['Nregistro'=>'2170827','nombre'=>'Julio','paterno'=>'Cespedes','materno'=>'Guardia','email'=>'alv8@gmail.com','telefono'=>'23532','direccion'=>'Montero Barrio 25 enero']);
-        Estudiante::create(['Nregistro'=>'2170828','nombre'=>'Sandro','paterno'=>'Duarte','materno'=>'Mamani','email'=>'alv99@gmail.com','telefono'=>'23523','direccion'=>'Santa Cruz La Cuchilla']);
+        Estudiante::create(['Nregistro'=>'217082911','nombre'=>'Leonel','paterno'=>'Chana','materno'=>'Cruz','email'=>'leonel@gmail.com','telefono'=>'2432423','direccion'=>'santa cruz']);
+        Estudiante::create(['Nregistro'=>'217083912','nombre'=>'Lurdes','paterno'=>'Contreras','materno'=>'Alvares','email'=>'lurdes@gmail.com','telefono'=>'3542354','direccion'=>'santa cruz']);
+        Estudiante::create(['Nregistro'=>'217082113','nombre'=>'Jazmin','paterno'=>'Leon','materno'=>'Castillo','email'=>'jasmin@gmail.com','telefono'=>'7235243','direccion'=>'santa cruz']);
+        Estudiante::create(['Nregistro'=>'217082214','nombre'=>'Noelia','paterno'=>'Duarte','materno'=>'Castro','email'=>'noelia@gmail.com','telefono'=>'7277323','direccion'=>'santa cruz']);
+        Estudiante::create(['Nregistro'=>'217082315','nombre'=>'Julio','paterno'=>'Guzman','materno'=>'Mendez','email'=>'julio@gmail.com','telefono'=>'7453232','direccion'=>'santa cruz']);
+        Estudiante::create(['Nregistro'=>'217082416','nombre'=>'Tereza','paterno'=>'Espinoza','materno'=>'alvarez','email'=>'teresa@gmail.com','telefono'=>'7252342','direccion'=>'santa cruz']);
+        Estudiante::create(['Nregistro'=>'217082517','nombre'=>'Marcos','paterno'=>'Condori','materno'=>'','email'=>'marcos@gmail.com','telefono'=>'72543523','direccion'=>'santa cruz']);
+        Estudiante::create(['Nregistro'=>'217082618','nombre'=>'Selena','paterno'=>'Nina','materno'=>'','email'=>'selena@gmail.com','telefono'=>'77323523','direccion'=>'santa cruz']);
+        Estudiante::create(['Nregistro'=>'221120459','nombre'=>'Julio','paterno'=>'Cespedes','materno'=>'','email'=>'julio@gmail.com','telefono'=>'7723532','direccion'=>'montero bariio 35ener']);
+        Estudiante::create(['Nregistro'=>'221120459','nombre'=>'Sandro','paterno'=>'Duarte','materno'=>'','email'=>'sandro@gmail.com','telefono'=>'7523523','direccion'=>'santa cruz']);
         
-     /*    Estudiante::create(['nombre'=>'alexia','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
-        Estudiante::create(['nombre'=>'raul','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
-        Estudiante::create(['nombre'=>'lucas','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
-        Estudiante::create(['nombre'=>'maria','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
-        Estudiante::create(['nombre'=>'lesly','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
-        Estudiante::create(['nombre'=>'veronica','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
-        Estudiante::create(['nombre'=>'sandra','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
-        Estudiante::create(['nombre'=>'leidy','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
-        Estudiante::create(['nombre'=>'luis','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
-        Estudiante::create(['nombre'=>'carlos','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
-        */
-        /*  
+        Estudiante::create(['Nregistro'=>'3217082122','nombre'=>'Alexia','paterno'=>'Mendoza','materno'=>'Prado','email'=>'medoza@gmail.com','telefono'=>'75676123','direccion'=>'montero barrio 25 de enero ']);
+        Estudiante::create(['Nregistro'=>'3217083922','nombre'=>'Raul','paterno'=>'Mendes','materno'=>'Prado','email'=>'meneces@gmail.com','telefono'=>'76564435','direccion'=>'montero barrio 25 de enero ']);
+        Estudiante::create(['Nregistro'=>'3217082122','nombre'=>'Lucas','paterno'=>'Suarez','materno'=>'Mejia','email'=>'lucas@gmail.com','telefono'=>'76574564','direccion'=>'montero barrio 25 de enero ']);
+        Estudiante::create(['Nregistro'=>'3217082232','nombre'=>'Maria','paterno'=>'Campos','materno'=>'Rivera','email'=>'maria@gmail.com','telefono'=>'65755633','direccion'=>'montero barrio 25 de enero ']);
+        Estudiante::create(['Nregistro'=>'3217082342','nombre'=>'lesly','paterno'=>'Alcoba','materno'=>'Mamani','email'=>'lesly@gmail.com','telefono'=>'7456456','direccion'=>'montero barrio urqupina']);
+        Estudiante::create(['Nregistro'=>'3217082432','nombre'=>'Veronica','paterno'=>'Canaviri','materno'=>'','email'=>'veronica@gmail.com','telefono'=>'74564563','direccion'=>'montero barrio urqupina']);
+        Estudiante::create(['Nregistro'=>'3231708254','nombre'=>'Sandra','paterno'=>'Cabrera','materno'=>'','email'=>'sandra@gmail.com','telefono'=>'764345322','direccion'=>'montero barrio urqupina']);
+        Estudiante::create(['Nregistro'=>'3217082622','nombre'=>'Leidy','paterno'=>'Munos','materno'=>'Cruz','email'=>'leidy@gmail.com','telefono'=>'745543454','direccion'=>'montero barrio urqupina']);
+        Estudiante::create(['Nregistro'=>'3221120459','nombre'=>'Luis','paterno'=>'Vargas','materno'=>'Gutierrez','email'=>'luis@gmail.com','telefono'=>'7576457','direccion'=>'montero barrio urqupina']);
+        Estudiante::create(['Nregistro'=>'3221120459','nombre'=>'Carlos','paterno'=>'Moreno','materno'=>'Azogue','email'=>'carlos@gmail.com','telefono'=>'745634','direccion'=>'montero barrio urqupina']);
+            /*  
         Estudiante::create(['nombre'=>'','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
         Estudiante::create(['nombre'=>'','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
         Estudiante::create(['nombre'=>'','paterno'=>'','materno'=>'','email'=>'alv@gmail.com','telefono'=>'','direccion'=>'']);
