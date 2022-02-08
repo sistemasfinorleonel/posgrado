f44ea4c80495d5a1ec1e293ecbca91b2366975c2
sistemasfinorleonel <ingenierosistemasfinor@gmail.com>
2022-02-07 21:31:04 -0400

    s

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -7,35 +7,19 @@ use Illuminate\Http\Request;
 
 class EstudianteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function index()
     {
         $estudiantes=Estudiante::paginate(12);
         return view('admin.estudiantes.index',compact('estudiantes'));
     
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+ public function create()
     {
         return view('admin.estudiantes.create');    
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $datosestudiante= request()->except('_token');
@@ -43,50 +27,22 @@ class EstudianteController extends Controller
         Estudiante::insert($datosestudiante);
         return redirect()->route('admin.estudiantes.index');   
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(Estudiante $estudiante)
     {
         return view('admin.estudiantes.show',compact('estudiante'));  
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Estudiante $estudiante)
     {
         return view('admin.estudiantes.edit',compact('estudiante'));
     
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Estudiante $estudiante)
     {
         $datosestudiante= request()->except(['_token','_method']);
         Estudiante::where('id','=',$estudiante->id)->update($datosestudiante);
         return redirect()->route('admin.estudiantes.index');
         }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Estudiante $estudiante)
     {
         $estudiante->delete();
